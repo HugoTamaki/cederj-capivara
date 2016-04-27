8e418409bb8afe9d1cb5b995503fda5fdda89e8b
Hugo Tamaki <hugokt2005@yahoo.com.br>
2016-04-27 05:55:59 -0300

    Adds angular file

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
+//= require jquery_ujs
+//= require angular/angular
 //= require www/app
 //= require www/controllers/app_controller
 //= require_tree .
