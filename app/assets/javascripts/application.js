5d25424fa35fe84607860320bba719786d3e6deb
Hugo Tamaki <hugokt2005@yahoo.com.br>
2016-04-29 14:51:57 -0300

    fixing angular and ui-router paths on application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,8 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require angular/angular.min
-//= require angular-ui-router/release/angular-ui-router.min
+//= require angular/angular
+//= require angular-ui-router/release/angular-ui-router
 //= require angular-rails-templates
 //= require_tree ../templates
 //= require www/app
