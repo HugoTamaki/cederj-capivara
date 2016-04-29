ee3ea3a44f4b7b8b417faf06e37d049327b9a782
Hugo Tamaki <hugokt2005@yahoo.com.br>
2016-04-29 09:36:12 -0300

    Adding api creation when creating user

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,23 +1,22 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   before_filter :configure_permitted_parameters, if: :devise_controller?
   skip_before_filter :verify_authenticity_token, :only => :create
+  respond_to :json
   
   # POST /resource
   def create
-    binding.pry
     build_resource(sign_up_params)
 
     resource.save
     yield resource if block_given?
     if resource.persisted?
       if resource.active_for_authentication?
-        set_flash_message! :notice, :signed_up
         sign_up(resource_name, resource)
-        respond_with resource, location: after_sign_up_path_for(resource)
+        resource.set_api_key
+        render json: {user: resource}
       else
-        set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
         expire_data_after_sign_in!
-        respond_with resource, location: after_inactive_sign_up_path_for(resource)
+        respond_with resource
       end
     else
       clean_up_passwords resource
