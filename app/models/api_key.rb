74a325cf667511fe0f2b2b5b1ab0ec5c431082f1
Hugo Tamaki <hugokt2005@yahoo.com.br>
2016-04-29 09:35:48 -0300

    Setting expiration on api creation and adding dependency on user

--- app/models/api_key.rb
+++ app/models/api_key.rb
@@ -1,12 +1,16 @@
 require 'securerandom'
 class ApiKey < ActiveRecord::Base
-  before_create :set_secret_and_token
-  belongs_to :user
+  before_create :set_secret_and_token, :set_expiration
+  belongs_to :user, dependent: :destroy
 
   private
 
   def set_secret_and_token
     self.secret = SecureRandom.uuid.gsub(/\-/,'')
     self.key = SecureRandom.uuid.gsub(/\-/,'')
+  end
+
+  def set_expiration
+    self.expires_at = Time.now + 7.days
   end
 end
