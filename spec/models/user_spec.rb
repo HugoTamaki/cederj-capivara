fa2742f958792de8e3309f979b63259e07bf0237
Hugo Tamaki <hugokt2005@yahoo.com.br>
2016-04-30 17:24:04 -0300

    User and Api Key unit tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe User do
+  let(:user) { FactoryGirl.create(:user) }
+
+  describe :attributes do
+    it { expect(user).to have_attribute(:first_name) }
+    it { expect(user).to have_attribute(:last_name) }
+    it { expect(user).to have_attribute(:email) }
+  end
+
+  describe :relationships do
+    it { expect(user).to respond_to(:api_keys) }
+  end
+
+  describe :methods do
+    describe '#set_api_key' do
+      it 'user should have new ApiKey' do
+        expect(ApiKey.count).to eql(0)
+        api_key = user.set_api_key
+        expect(ApiKey.count).to eql(1)
+        expect(api_key.user).to eql(user)
+      end
+    end
+  end
 end
