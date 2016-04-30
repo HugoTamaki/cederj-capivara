35a3e52c1e7b3904e007d813a022b7008e9cdaf1
Hugo Tamaki <hugokt2005@yahoo.com.br>
2016-04-30 16:48:41 -0300

    Creates action for login

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,6 +1,26 @@
 class Users::SessionsController < Devise::SessionsController
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+  skip_before_filter :verify_authenticity_token, :only => :create
+  respond_to :json
+
+  # POST /resource/sign_in
+  def create
+    resource = User.find_by(email: params[:api_v1_user][:email])
+    if resource.valid_password?(params[:api_v1_user][:password])
+      api_key = renew_api_key(resource)
+      render json: {user: resource, api_key: {token: api_key.token}}
+    else
+      render json: {error: 'email or password is wrong'}, status: 404
+    end
+  end
+
+  private
+
+  def renew_api_key(resource)
+    api_key = resource.api_keys.last
+    if api_key.expired?
+      api_key.destroy
+      api_key = resource.set_api_key
+    end
+    api_key
+  end
 end
