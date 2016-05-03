37139d462c0e8612071dcb7ffc1c72c42278e32d
Hugo Tamaki <hugokt2005@yahoo.com.br>
2016-05-03 17:58:22 -0300

    Adding sign out action to api

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,28 +1,27 @@
 class Users::SessionsController < Devise::SessionsController
-  skip_before_filter :verify_authenticity_token, :only => :create
+  skip_before_filter :verify_authenticity_token, :only => [:create, :destroy]
+  skip_before_filter :verify_signed_out_user
   respond_to :json
+
+  before_action :authenticate, only: [:destroy]
 
   # POST /resource/sign_in
   def create
     resource = User.find_by(email: params[:api_v1_user][:email])
     if resource && resource.valid_password?(params[:api_v1_user][:password])
-      api_key = renew_api_key(resource)
+      api_key = resource.set_api_key
       render json: format_response(resource, api_key)
     else
       render json: {error: 'email or password is wrong'}, status: 404
     end
   end
 
-  private
-
-  def renew_api_key(resource)
-    api_key = resource.api_keys.last
-    if api_key.expired?
-      api_key.destroy
-      api_key = resource.set_api_key
-    end
-    api_key
+  # DELETE /resource/sign_out
+  def destroy
+    # find and destroy ApiKey
   end
+
+  private
 
   def format_response(resource, api_key)
     {
@@ -33,5 +32,22 @@ class Users::SessionsController < Devise::SessionsController
       },
       api_key: api_key.token
     }
+  end
+
+  protected
+
+  def authenticate
+    authenticate_token || render_unauthorized
+  end
+
+  def authenticate_token
+    authenticate_with_http_token do |token, options|
+
+    end
+  end
+
+  def render_unauthorized
+    self.headers['WWW-Authenticate'] = 'Token realm="Application"'
+    render json: 'Bad credentials', status: 401
   end
 end
