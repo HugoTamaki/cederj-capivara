2275086d8c727f3ddc98309d1a02b2120d840a98
Hugo Tamaki <hugokt2005@yahoo.com.br>
2016-05-14 22:09:31 -0300

    Fixing specs and adding update user attr

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,7 +1,10 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   before_filter :configure_permitted_parameters, if: :devise_controller?
-  skip_before_filter :verify_authenticity_token, :only => :create
+  skip_before_filter :verify_authenticity_token, :only => [:create]
+  skip_before_filter :authenticate_scope!, only: [:update]
   respond_to :json
+
+  before_action :authenticate, only: [:update]
 
   # POST /resource
   def create
@@ -15,10 +18,21 @@ class Users::RegistrationsController < Devise::RegistrationsController
     end
   end
 
+  def update
+    resource = get_resource
+    resource_updated = update_resource(resource, account_update_params)
+    if resource_updated
+      render json: format_response(resource, @api_key), status: 200
+    else
+      render json: {error: resource.errors}, status: 400
+    end
+  end
+
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :email, :password, :password_confirmation])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :password, :password_confirmation, :current_password])
   end
 
   def format_response(resource, api_key)
@@ -30,5 +44,26 @@ class Users::RegistrationsController < Devise::RegistrationsController
       },
       api_key: api_key.token
     }
+  end
+
+  def authenticate
+    authenticate_token || render_unauthorized
+  end
+
+  def authenticate_token
+    authenticate_with_http_token do |token, options|
+      secret, key = token.split(':')
+      @api_key = ApiKey.find_by(secret: secret, key: key)
+      @api_key && @api_key.not_expired? ? true : false
+    end
+  end
+
+  def get_resource
+    @api_key.user
+  end
+
+  def render_unauthorized
+    self.headers['WWW-Authenticate'] = 'Token realm="Application"'
+    render json: {error: 'Bad credentials'}, status: 401
   end
 end
--- spec/controllers/users/registrations_controller_spec.rb
+++ spec/controllers/users/registrations_controller_spec.rb
@@ -7,33 +7,128 @@ describe Users::RegistrationsController do
   end
 
   describe 'POST #create' do
-    it 'creates user and send user info and token' do
-      post :create, {
-        api_v1_user: {
-          first_name: 'John',
-          last_name: 'Doe',
-          email: 'johndoe@email.com',
-          password: '123123123',
-          password_confirmation: '123123123'
+    context 'valid' do
+      it 'creates user and send user info and token' do
+        post :create, {
+          api_v1_user: {
+            first_name: 'John',
+            last_name: 'Doe',
+            email: 'johndoe@email.com',
+            password: '123123123',
+            password_confirmation: '123123123'
+          }
         }
-      }
 
-      user = User.last
-      api_key = ApiKey.last
-      expected_reponse = {
-        user: {
-          first_name: 'John',
-          last_name: 'Doe',
-          email: 'johndoe@email.com'
-        },
-        api_key: api_key.token
-      }
+        user = User.last
+        api_key = ApiKey.last
+        expected_reponse = {
+          user: {
+            first_name: 'John',
+            last_name: 'Doe',
+            email: 'johndoe@email.com'
+          },
+          api_key: api_key.token
+        }
 
-      expect(user.first_name).to eql('John')
-      expect(user.last_name).to eql('Doe')
-      expect(user.email).to eql('johndoe@email.com')
-      expect(user.api_keys.count).to eql(1)
-      expect(response.body).to eql(expected_reponse.to_json)
+        expect(user.first_name).to eql('John')
+        expect(user.last_name).to eql('Doe')
+        expect(user.email).to eql('johndoe@email.com')
+        expect(user.api_keys.count).to eql(1)
+        expect(response.body).to eql(expected_reponse.to_json)
+      end
+    end
+
+    context 'not valid' do
+      context 'email is already taken' do
+        let!(:user) { FactoryGirl.create(:user, email: 'johndoe@email.com') }
+
+        it 'does not create user as email is the same' do
+          post :create, {
+            api_v1_user: {
+              first_name: 'John',
+              last_name: 'Doe',
+              email: 'johndoe@email.com',
+              password: '123123123',
+              password_confirmation: '123123123'
+            }
+          }
+
+          expected_reponse = {
+            error: {
+              email: ['has already been taken']
+            }
+          }
+
+          expect(response.body).to eql(expected_reponse.to_json)
+          expect(User.count).to eql(1)
+        end
+      end
+    end
+
+    context 'attributes are not present' do
+      it 'sends cant be blank error' do
+        post :create, {
+          api_v1_user: {
+            first_name: '',
+            last_name: '',
+            email: '',
+            password: '',
+            password_confirmation: ''
+          }
+        }
+
+        expected_reponse = {
+          error: {
+            email: ["can't be blank"],
+            password: ["can't be blank"],
+            first_name: ["can't be blank"],
+            last_name: ["can't be blank"]
+          }
+        }
+
+        expect(response.body).to eql(expected_reponse.to_json)
+        expect(User.count).to eql(0)
+      end
+    end
+  end
+
+  describe 'PUT #update' do
+    context 'valid' do
+      let!(:user) { FactoryGirl.create(:user, email: 'johndoe@email.com', first_name: 'John', last_name: 'Doe', password: '123123123', password_confirmation: '123123123') }
+      let!(:api_key) { FactoryGirl.create(:api_key, user: user, expires_at: Time.now + 7.days) }
+
+      it 'updates user and send user info and token' do
+        request.env['HTTP_AUTHORIZATION'] = "Token token=#{api_key.token}"
+
+        antique_password = user.encrypted_password
+
+        expect(user.first_name).to eql('John')
+        expect(user.last_name).to eql('Doe')
+
+        put :update, {
+          api_v1_user: {
+            first_name: 'Jonhson',
+            last_name: 'Doan',
+            current_password: '123123123',
+            password: '321321321',
+            password_confirmation: '321321321'
+          }
+        }
+
+        expected_reponse = {
+          user: {
+            first_name: 'Jonhson',
+            last_name: 'Doan',
+            email: 'johndoe@email.com'
+          },
+          api_key: api_key.token
+        }
+
+        expect(response.body).to eql(expected_reponse.to_json)
+        expect(user.reload.encrypted_password).not_to eql(antique_password)
+        expect(user.reload.first_name).to eql('Jonhson')
+        expect(user.reload.last_name).to eql('Doan')
+      end
     end
   end
 end
