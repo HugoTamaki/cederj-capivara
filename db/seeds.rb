e34a7a801bd7db7d51cfe9b028c4eca5d7c9a5d7
Hugo Tamaki <hugokt2005@yahoo.com.br>
2016-05-21 15:43:25 -0300

    Discipline seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+disciplines = [
+  {
+    name: 'Projeto de Desenvolvimento de Algoritmos',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium voluptates consequatur, consequuntur laboriosam fugit sunt cupiditate, optio quas incidunt similique, eius vel accusantium nam. Ipsam rerum enim adipisci deleniti non!'
+  },
+  {
+    name: 'Construção de Páginas Web',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni ad fugiat quisquam qui cumque. Vel, iusto, voluptates. Sint possimus saepe, sit recusandae officia sed aut et quis nisi deleniti omnis?'
+  },
+  {
+    name: 'Estrutura de Dados',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi asperiores dolor consequatur corporis quod esse id saepe laborum sit nemo repellat, inventore, voluptatibus maiores reiciendis beatae itaque totam quidem, rerum.'
+  }
+]
+
+disciplines.each do |discipline|
+  Discipline.create(name: discipline[:name], description: discipline[:description])
+end
