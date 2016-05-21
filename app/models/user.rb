2569ddcd782756acd41321bcc56fd3db6169ece7
Hugo Tamaki <hugokt2005@yahoo.com.br>
2016-05-21 15:43:13 -0300

    Defines discipline relationship and associates disciplines on creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,25 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :api_keys, dependent: :destroy
+  has_many :user_disciplines
+  has_many :disciplines, through: :user_disciplines
 
   validates :first_name, presence: true
   validates :last_name, presence: true
 
+  before_create :set_disciplines
+
   def set_api_key
     self.api_keys.create
+  end
+
+  private
+
+  def set_disciplines
+    disciplines = Discipline.all
+
+    disciplines.each do |discipline|
+      self.disciplines << discipline
+    end
   end
 end
