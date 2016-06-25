7b57318b00f4d36697084e30696e673cec6fa320
Hugo Tamaki <hugokt2005@yahoo.com.br>
2016-06-25 10:31:11 -0300

    Creates default authorization response for pundit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   include Pundit
   protect_from_forgery with: :null_session
+
+  rescue_from Pundit::NotAuthorizedError, with: :not_authorized
+
+  private
+
+  def not_authorized
+    render json: { error: 'Not authorized' }, status: 401
+  end
 end
