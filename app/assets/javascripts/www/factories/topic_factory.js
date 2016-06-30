651d17a1f100b5477e037c4251898917f1f7e2b5
Hugo Tamaki <hugokt2005@yahoo.com.br>
2016-06-29 22:02:51 -0300

    Refactors topic factory

--- app/assets/javascripts/www/factories/topic_factory.js
+++ app/assets/javascripts/www/factories/topic_factory.js
@@ -3,9 +3,10 @@ app.factory('Topic', [
   function () {
 
     function Topic(options) {
-      this.id = options.id,
-      this.name = options.name,
-      this.content = options.content,
+      this.id = options.id
+      this.name = options.name
+      this.content = options.content
+      this.room_id = options.room_id
       this.messages = []
     }
 
