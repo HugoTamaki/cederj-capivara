8c1d4587a384c50a73e692d3f4f7440bfb8edbbb
Hugo Tamaki <hugokt2005@yahoo.com.br>
2016-06-29 22:04:02 -0300

    Message unitary specs

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe Message, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Message do
+  let(:user)  { FactoryGirl.create(:user) }
+  let(:room)  { FactoryGirl.create(:room, user: user) }
+  let(:topic) { FactoryGirl.create(:topic, room: room, user: user) }
+  let(:message) { FactoryGirl.create(:message, topic: topic, user: user) }
+
+  describe :attributes do
+    it { expect(message).to have_attribute(:content) }
+    it { expect(message).to have_attribute(:topic_id) }
+    it { expect(message).to have_attribute(:user_id) }
+  end
+
+  describe :relationships do
+    it { expect(message).to respond_to(:topic) }
+    it { expect(message).to respond_to(:user) }
+  end
 end
