63c142d9864e7248ecb2ecb00e8ec048d9f35a4b
Hugo Tamaki <hugokt2005@yahoo.com.br>
2016-07-06 21:32:02 -0300

    Adding dependency destruction to room topics

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,6 +1,6 @@
 class Room < ActiveRecord::Base
   belongs_to :user
-  has_many :topics
+  has_many :topics, dependent: :destroy
   has_many :room_users
   has_many :participants, through: :room_users, class_name: 'User'
 
