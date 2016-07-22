5364e1095958fa726ad824b761764634b6680a0c
Hugo Tamaki <hugokt2005@yahoo.com.br>
2016-07-21 22:54:40 -0300

    Validates presence of room, sender and receiver

--- app/models/room_entry_request.rb
+++ app/models/room_entry_request.rb
@@ -2,4 +2,8 @@ class RoomEntryRequest < ActiveRecord::Base
   belongs_to :room
   belongs_to :sender, class_name: 'User'
   belongs_to :receiver, class_name: 'User'
+
+  validates :room_id, presence: true
+  validates :sender_id, presence: true
+  validates :receiver_id, presence: true
 end
--- spec/models/room_entry_request_spec.rb
+++ spec/models/room_entry_request_spec.rb
@@ -12,11 +12,62 @@ describe RoomEntryRequest do
     it { expect(room_entry_request).to have_attribute(:sender_id) }
     it { expect(room_entry_request).to have_attribute(:receiver_id) }
     it { expect(room_entry_request).to have_attribute(:room_id) }
+    it { expect(room_entry_request).to have_attribute(:accepted) }
+    it { expect(room_entry_request).to have_attribute(:token) }
   end
 
   describe :relationships do
     it { expect(room_entry_request).to respond_to(:room) }
     it { expect(room_entry_request).to respond_to(:sender) }
     it { expect(room_entry_request).to respond_to(:receiver) }
+  end
+
+  describe :validations do
+    context :valid do
+      describe 'All params are there' do
+        it 'Creates successfully room_entry_request' do
+          room_entry_request.sender = sender
+          room_entry_request.receiver = receiver
+          room_entry_request.room = room
+
+          expect(room_entry_request.save).to eql(true)
+          expect(room_entry_request.room).to eql(room)
+          expect(room_entry_request.sender).to eql(sender)
+          expect(room_entry_request.receiver).to eql(receiver)
+        end
+      end
+    end
+
+    context :invalid do
+      describe 'It lacks room_id' do
+        it 'fails room_entry_request creation' do
+          room_entry_request.sender = sender
+          room_entry_request.receiver = receiver
+          room_entry_request.room = nil
+
+          expect(room_entry_request.save).to eql(false)
+        end
+      end
+
+      describe 'It lacks sender_id' do
+        it 'fails room_entry_request creation' do
+          room_entry_request.sender = nil
+          room_entry_request.receiver = receiver
+          room_entry_request.room = room
+
+          expect(room_entry_request.save).to eql(false)
+        end
+      end
+
+      describe 'It lacks receiver_id' do
+        it 'fails room_entry_request creation' do
+          room_entry_request.sender = sender
+          room_entry_request.receiver = nil
+          room_entry_request.room = room
+
+          expect(room_entry_request.save).to eql(false)
+        end
+      end
+    end
   end
 end
