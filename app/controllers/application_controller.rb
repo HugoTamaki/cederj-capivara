d24e016901032a1895ed99271e53c4f6c4255ca7
Hugo Tamaki <hugokt2005@yahoo.com.br>
2016-08-16 18:31:37 -0300

    Adjusting position of commands

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,13 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  include Pundit
+
   before_action :authenticate
 
-  include Pundit
   protect_from_forgery with: :null_session
 
   rescue_from Pundit::NotAuthorizedError, with: :not_authorized
-
   rescue_from ::ActiveRecord::RecordNotFound, with: :record_not_found
 
   private
