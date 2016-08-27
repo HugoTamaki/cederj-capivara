13747953326774a142281cdc5c1a1c88d5c500a8
cyprianoeduardo <cypriano.eduardo@gmail.com>
2016-08-27 15:16:11 -0300

    Added created-at and updated-at attribute on Message Factory

--- app/assets/javascripts/www/factories/message_factory.js
+++ app/assets/javascripts/www/factories/message_factory.js
@@ -8,6 +8,8 @@ app.factory('Message', [
       this.content = options.content,
       this.topic_id = options.topic_id
       this.author = options.user
+      this.created_at = options.created_at
+      this.updated_at = options.updated_at
 
       this.authorFullName = function () {
         return this.author.first_name + ' ' + this.author.last_name
@@ -15,6 +17,14 @@ app.factory('Message', [
 
       this.authorIsUser = function () {
         return User.id === this.author.id
+      }
+
+      this.createdDate = function () {
+        if (moment(this.created_at).add(3, 'days') > moment()) {
+          return moment(this.created_at).fromNow()
+        } else {
+          return moment(this.created_at).format('DD/MM/YY')
+        }
       }
     }
 
