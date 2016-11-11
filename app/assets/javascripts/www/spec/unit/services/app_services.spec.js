da4544e0194931bb9a8d18cc8edeb97982c50c0a
Hugo Tamaki <hugokt2005@yahoo.com.br>
2016-11-11 18:21:55 -0200

    Adding StartupService specs

--- app/assets/javascripts/www/spec/unit/services/app_services.spec.js
+++ app/assets/javascripts/www/spec/unit/services/app_services.spec.js
@@ -62,10 +62,90 @@ describe('LabelService', function() {
   }));
 
   describe('#error', function() {
-    describe('failed login', function() {
-      it('shows Email ou senha errados.', function() {
-        expect(LabelService.error.failedLogin).toEqual('Email ou senha errados.');
+    it('failedLogin', function() {
+      expect(LabelService.error.failedLogin).toEqual('Email ou senha errados.');
+    });
+
+    it('somethingWrong', function() {
+      expect(LabelService.error.somethingWrong).toEqual('Alguma coisa aconteceu, tente novamente mais tarde.');
+    });
+
+    it('alreadyToken', function() {
+      expect(LabelService.error.alreadyTaken).toEqual('Já existe um usuário com este email.');
+    });
+  });
+
+  describe('#notification', function() {
+    describe('profileEdit', function() {
+      it('success', function() {
+        expect(LabelService.notification.profileEdit.success).toEqual('Usuário editado com sucesso.');
       });
+    });
+
+    describe('roomDelete', function() {
+      it('success', function() {
+        expect(LabelService.notification.roomDelete.success).toEqual('Sala de discussão apagada com sucesso.');
+      });
+    });
+
+    describe('topicDelete', function() {
+      it('success', function() {
+        expect(LabelService.notification.topicDelete.success).toEqual('Tópico apagado com sucesso.');
+      });
+    });
+
+    describe('roomEntryRequest', function() {
+      describe('invitation', function() {
+        it('success', function() {
+          expect(LabelService.notification.roomEntryRequest.invitation.success).toEqual('Pedido enviado com sucesso.');
+        });
+      });
+
+      describe('accepted', function() {
+        it('success', function() {
+          expect(LabelService.notification.roomEntryRequest.accepted.success).toEqual('Pedido aceito com sucesso.');
+        });
+      });
+    });
+  });
+});
+
+describe('StartupService', function() {
+  var StartupService, User;
+
+  beforeEach(module('capivara'));
+
+  beforeEach(inject(function(_StartupService_, _User_) {
+    User = _User_
+    StartupService = _StartupService_;
+    StartupService.tasks = {
+      userInit: function() {}
+    }
+  }));
+
+  describe('#init', function() {
+    beforeEach(function() {
+      StartupService.startTasks = undefined;
+    });
+
+    it('defines startTasks', function() {
+      expect(StartupService.startTasks).toBe(undefined);
+
+      StartupService.init();
+
+      expect(StartupService.startTasks).toEqual(['userInit']);
+    });
+  });
+
+  describe('#executeTask', function() {
+    beforeEach(function() {
+      spyOn(StartupService.tasks, 'userInit');
+    });
+
+    it('executes task', function() {
+      StartupService.executeTask('userInit');
+
+      expect(StartupService.tasks.userInit).toHaveBeenCalled();
     });
   });
 });
